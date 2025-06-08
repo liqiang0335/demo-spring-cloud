1d11371bfbed31b3ef2c09ac45fd84a014bb3348
liqiang0335 <liqiang0335@qq.com>
2025-06-08 21:09:27 +0800

    add

---
+++ server02/src/main/kotlin/com/demo/server02/Server02Application.kt
@@ -0,0 +1,11 @@
+package com.demo.server02
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Server02Application
+
+fun main(args: Array<String>) {
+    runApplication<Server02Application>(*args)
+}
---
+++ server02/src/test/kotlin/com/demo/server02/Server02ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.demo.server02
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Server02ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
